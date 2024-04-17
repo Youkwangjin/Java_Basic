cb404dd2e8e6aa3f2cac78fc58dc99d0405d09e3
Kwangjin <kwangjin5468@naver.com>
2024-04-17 22:23:15 +0900

    4/17 java Study

---
+++ src/study2/class2/ClassStart4.java
@@ -0,0 +1,21 @@
+package study2.class2;
+
+public class ClassStart4 {
+
+    /**
+     * 대원칙 : 자바의 대입은 항상 변수의 "값"을 복사해서 대입한다.
+     */
+
+    public static void main(String[] args) {
+        Data dataA = new Data(); // ex) dataA = x001
+        dataA.value = 10;
+        System.out.println("메서드 호출 전 dataA.value = " + dataA.value); // 10
+        changeRefer(dataA);
+        System.out.println("메서드 호출 후 dataA.value = " + dataA.value); // 10
+
+    }
+
+    public static void changeRefer(Data dataX) {
+        dataX.value = 20;
+    }
+}
---
+++ src/study2/class2/Data.java
@@ -0,0 +1,7 @@
+package study2.class2;
+
+public class Data {
+
+    int value = 10;
+
+}
