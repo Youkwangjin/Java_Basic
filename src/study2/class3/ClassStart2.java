a8a821a181aa44084f7646ebb1dea06e0aa4910d
Kwangjin <kwangjin5468@naver.com>
2024-04-18 23:18:24 +0900

    4/18 java Study

---
+++ src/study2/class3/ClassStart2.java
@@ -0,0 +1,59 @@
+package study2.class3;
+
+public class ClassStart2 {
+
+    public static void main(String[] args) {
+        // 객체 지향적 프로그래밍
+        MusicPlayerData data = new MusicPlayerData();
+        data.volume = 1;
+        data.isOn = true;
+
+        // 음악 플레이어 켜기
+        soundOn(data);
+
+        // 볼륨 증가
+        volumeUp(data);
+
+        // 볼륨 증가
+        volumeUp(data);
+
+        // 볼륨 감소
+        volumeDown(data);
+
+        // 음악 플레이어 선택
+        showStates(data);
+
+        // 음악 플레이어 끄기
+        data.isOn = false;
+        System.out.println("음악 플레이어를 종료합니다.");
+    }
+
+    static void soundOn(MusicPlayerData data) {
+        System.out.println("음악 플레이어를 시작합니다.");
+        data.isOn = true;
+    }
+
+    static void soundOff(MusicPlayerData data) {
+        System.out.println("음악 플레이어를 종료합니다.");
+        data.isOn = false;
+    }
+
+    static void volumeUp(MusicPlayerData data) {
+        data.volume ++;
+        System.out.println("음악 플레이어 볼륨 : " + data.volume);
+    }
+
+    static void volumeDown(MusicPlayerData data) {
+        data.volume --;
+        System.out.println("음악 플레이어 볼륨 : " + data.volume);
+    }
+
+    static void showStates(MusicPlayerData data) {
+        System.out.println("음악 플레이어 상태 확인");
+        if (data.isOn) {
+            System.out.println("음악 플레이어 ON, 볼륨 : " + data.volume);
+        } else {
+            System.out.println("음악 플레이어 ON");
+        }
+    }
+}
---
+++ src/study2/class3/MusicPlayerData.java
@@ -0,0 +1,8 @@
+package study2.class3;
+
+public class MusicPlayerData {
+    
+    // 변수 초기화 작업
+    int volume = 0;
+    boolean isOn = false;
+}
